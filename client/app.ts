5ba4d02b4762216f1f6462897270d4d7164fe685
vimniky <vimniky@gmail.com>
2016-11-28 13:14:59 +0800

    Update src

--- client/app.ts
+++ client/app.ts
@@ -1,6 +1,6 @@
-import {VNode, div} from '@cycle/dom'
-import {DOMSource} from '@cycle/dom/xstream-typings'
-import xs, {Stream} from 'xstream'
+import { VNode, div } from '@cycle/dom'
+import { DOMSource } from '@cycle/dom/xstream-typings'
+import xs, { Stream } from 'xstream'
 
 export type Sources = {
   DOM: DOMSource
@@ -10,9 +10,9 @@ export type Sinks = {
   DOM: Stream<VNode>
 }
 
-export function App (sources: Sources): Sinks {
+export function App(sources: Sources): Sinks {
   const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
+    div('.myClass', { style: { color: 'red' } }, 'My Awesome Cycle.js app')
   )
   const sinks = {
     DOM: vtree$
